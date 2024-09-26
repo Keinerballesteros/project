640a0eeed36d59c9da07a8bd08ad21773cd9a6bb
Keinerballesteros <kballesterosm@ufpso.edu.co>
2024-09-26 15:38:44 -0500

    load

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\GaleriasController;
+use App\Http\Controllers\CategoriasController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +14,28 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+Route::get('/user/{id}', function ($id) {
+ return 'ID de usuario: ' . $id;
+})->where('id', '[0-9]{1,3}');
+
+
+
+Route::resource('productos', ProductoController::class);
+
+
+Route::resource('categoria',CategoriasController::class);
+Route::get('cambioestadocategoria',[CategoriasController::class,'cambioestadocategoria']) -> name('cambioestadocategoria');
+
+Route::resource('galeria',GaleriasController::class);
+Route::get('cambioestadogaleria',[GaleriasController::class,'cambioestadogaleria']) -> name('cambioestadogaleria');
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [HomeController::class, 'home']);
+    Route::get('/categoria', [CategoriasController::class, 'categoria']);
+    Route::get('/galeria', [CategoriasController::class, 'galeria']);
+    });
