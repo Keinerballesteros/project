0d6cb5aedd797f271c8419192184a8cc3c33cbb6
Keinerballesteros <kballesterosm@ufpso.edu.co>
2024-10-03 15:48:02 -0500

    load

--- resources/views/layouts/partial/footer.blade.php
+++ resources/views/layouts/partial/footer.blade.php
@@ -1,9 +1,9 @@
 <div class="wrapper d-flex flex-column min-vh-100"> 
     <footer class="main-footer mt-auto">
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+        <strong>Copyright &copy; 2014-2021 <img src="https://divisis.ufpso.edu.co/images/baner/logo_vertical.png" alt="logo" style="width:25px; height:25px"></strong>
         All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
-            <b>Version</b> 3.2.0
+            <b>Version</b> 1.0.0
         </div>
     </footer>
 </div>
